1c3e3d5ca235303cc622d9f9bf8657947ac6e50b
Leo Yang <leoyang0105@gmail.com>
2025-03-01 19:54:33 +0800

    add empty ctor for Pagination

--- src/Deepin.Infrastructure/Pagination/Pagination.cs
+++ src/Deepin.Infrastructure/Pagination/Pagination.cs
@@ -4,9 +4,12 @@ public class Pagination<T> : IPagination<T>
     public IEnumerable<T> Items { get; set; }
     public int Offset { get; set; }
     public int Limit { get; set; }
-    public int TotalCount { get; set; } 
-    public bool HasMore { get; set; } 
-
+    public int TotalCount { get; set; }
+    public bool HasMore { get; set; }
+    public Pagination()
+    {
+        Items = [];
+    }
     public Pagination(IQueryable<T> source, int offset, int limit)
     {
         int totalCount = source.Count();
